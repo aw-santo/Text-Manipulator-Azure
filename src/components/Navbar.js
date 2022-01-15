fe0abc87cd7ffb3ee6e9d3649c982fb7d1ba5afd
sanket_ghormode <sanghormode2@gmail.com>
2022-01-15 14:06:55 +0530

    removed routes ---/about for github pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,13 +19,13 @@ export default function Navbar( props ) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                {/* <a className="nav-link active" aria-current="page" href="/">Home</a> */}
-                <Link className="nav-link active" to="/" aria-current="page">Home</Link>
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                {/* <Link className="nav-link active" to="/" aria-current="page">Home</Link> */}
               </li>
-              <li className="nav-item">
+              {/* <li className="nav-item"> */}
                 {/* <a className="nav-link" href="about">{props.aboutText}</a> */}
-                <Link className="nav-link" to="/about">{props.aboutText}</Link>
-              </li>
+                {/* <Link className="nav-link" to="/about">{props.aboutText}</Link> */}
+              {/* </li> */}
               
             </ul>
             <div className={`form-check form-switch text-${props.mode === 'light' ? "dark" : "light"}`}>
